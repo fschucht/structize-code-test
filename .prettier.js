0cf245cc6f0b33ee2efd681be23b1d3de8e943cf
Fabian Schucht <fabianschucht@gmail.com>
2025-01-15 11:49:17 +0000

    Setup project

---
+++ .prettier.js
@@ -0,0 +1,10 @@
+
+/** @type {import("prettier").Config} */
+export default {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 120,
+  quoteProps: 'consistent',
+  arrowParens: 'avoid',
+}
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full min-h-[100vh]">
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  );
+}
