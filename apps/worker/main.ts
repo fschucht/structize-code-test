2d54478c1a62161177bb87fe8cfbec77b78ac324
Fabian Schucht <fabianschucht@gmail.com>
2025-01-16 15:02:19 +0000

    Fix eslint for worker app

--- apps/worker/main.ts
+++ apps/worker/main.ts
@@ -1,26 +1,30 @@
 import { Worker } from "bullmq";
-import { ObjectId } from 'mongodb'
-import { CALCULATIONS_QUEUE_NAME } from '@repo/calculations/constants'
+import { ObjectId } from "mongodb";
+import { CALCULATIONS_QUEUE_NAME } from "@repo/calculations/constants";
 import { redisConnection } from "@repo/redis/connection";
 import type { ComputeCalculationJob } from "@repo/calculations/jobs/computeCalculationJob";
 import { computeCalculation } from "@repo/calculations/services/calculations";
 
-const worker = new Worker<ComputeCalculationJob>(CALCULATIONS_QUEUE_NAME, async (job) => {
-  const calculationId = ObjectId.createFromHexString(job.data.calculationId)
-  await computeCalculation(calculationId)
-}, {
-  connection: redisConnection,
-  // For demonstration purposes, the concurrency is set to 2 here.
-  concurrency: 2,
-  autorun: false
-});
+const worker = new Worker<ComputeCalculationJob>(
+  CALCULATIONS_QUEUE_NAME,
+  async (job) => {
+    const calculationId = ObjectId.createFromHexString(job.data.calculationId);
+    await computeCalculation(calculationId);
+  },
+  {
+    connection: redisConnection,
+    // For demonstration purposes, the concurrency is set to 2 here.
+    concurrency: 2,
+    autorun: false,
+  },
+);
 
-process.on('exit', async () => {
+process.on("exit", async () => {
   if (!worker.isRunning()) {
-    return
+    return;
   }
 
-  await worker.close()
-})
+  await worker.close();
+});
 
-await worker.run()
+await worker.run();
