888dc38d2d59a3b33e763b3a848bb37e474b3115
Fabian Schucht <fabianschucht@gmail.com>
2025-01-16 15:11:58 +0000

    Fix mongoose overwrite model error

--- packages/calculations/src/models/calculation.ts
+++ packages/calculations/src/models/calculation.ts
@@ -1,7 +1,10 @@
-import { calculationSchema } from "../schemas/calculation.ts";
-import { mongoConnection } from "@repo/mongo/connection";
-
-export const calculationModel = mongoConnection.model(
-  "Calculation",
+import type { Model } from "mongoose";
+import {
   calculationSchema,
-);
+  type CalculationDocument,
+} from "../schemas/calculation.ts";
+import { mongooseConnection } from "@repo/mongo/connection";
+
+export const calculationModel: Model<CalculationDocument> =
+  (mongooseConnection.models.Calculation as Model<CalculationDocument>) ||
+  mongooseConnection.model("Calculation", calculationSchema);
--- packages/mongo/src/connection.ts
+++ packages/mongo/src/connection.ts
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
 import { config } from "./config.ts";
 
-export const mongoConnection = await mongoose.connect(config.MONGODB_URI);
+export const mongooseConnection = await mongoose.connect(config.MONGODB_URI);
